ea0f0e4bba488740ac42331a3c7644cd28d6b693
Alan Munoz <afer.mg@gmail.com>
2024-07-26 17:57:26 -0400

    reimplement useful pull requests

--- zenodo_upload.sh
+++ zenodo_upload.sh
@@ -10,6 +10,21 @@ VERBOSE=0
 if [ "$3" == "--verbose" ] || [ "$3" == "-v" ]; then
     VERBOSE=1
 fi
+# Test if ZENODO_TOKEN is defined
+
+if [ -z "$ZENODO_TOKEN " ]; then
+    echo "Please use the ZENODO_TOKEN environment variable to set your Zenodo API token."
+    echo  "You can create a new token at https://zenodo.org/account/settings/applications/tokens/new/"
+    exit 2
+fi
+
+# Test if the file provided is actually a file
+
+if [ ! -f "$2" ]; then
+    echo "The file $2 does not exist."
+    exit 2
+fi
+
 
 # strip deposition url prefix if provided; see https://github.com/jhpoelen/zenodo-upload/issues/2#issuecomment-797657717
 DEPOSITION=$( echo $1 | sed 's+^http[s]*://zenodo.org/deposit/++g' )
@@ -18,7 +33,15 @@ FILENAME=$(echo $FILEPATH | sed 's+.*/++g')
 FILENAME=${FILENAME// /%20}
 ZENODO_ENDPOINT=${ZENODO_ENDPOINT:-https://zenodo.org}
 
-BUCKET=$(curl ${ZENODO_ENDPOINT}/api/deposit/depositions/"$DEPOSITION"?access_token="$ZENODO_TOKEN" | jq --raw-output .links.bucket)
+BUCKET_DATA=$(curl "${ZENODO_ENDPOINT}/api/deposit/depositions/$DEPOSITION?access_token=$ZENODO_TOKEN")
+BUCKET=$(echo "$BUCKET_DATA" | jq --raw-output .links.bucket)
+
+if [ "$BUCKET" = "null" ]; then
+    echo
+    echo "Could not find URL for upload. Response from server:"
+    echo "$BUCKET_DATA"
+    exit 1
+fi
 
 if [ "$VERBOSE" -eq 1 ]; then
     echo "Deposition ID: $DEPOSITION"
